76e8d56f08e4a3139e432722db5f66ba506c86f0
Scrappy1cocco <jedediah.shine100@gmail.com>
2018-07-10 15:25:05 +0300

    Revert "window demo2"
    This reverts commit ecd0f53e3b38749b6c0b01b6cfdb4c396f032712.

--- nerds/js/slide.js
+++ nerds/js/slide.js
@@ -2,10 +2,3 @@ $('.owl-carousel').owlCarousel({
     loop:false,    
     items:1   
 })
-$(document).ready(function() {
-	    $("#content").fancybox({
-	        'titlePosition' : 'inside',
-	        'transitionIn' : 'fade',
-	        'transitionOut' : 'elastic'
-	    });
-	});
