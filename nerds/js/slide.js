ecd0f53e3b38749b6c0b01b6cfdb4c396f032712
Scrappy1cocco <jedediah.shine100@gmail.com>
2018-07-10 10:45:03 +0300

    window demo2

--- nerds/js/slide.js
+++ nerds/js/slide.js
@@ -2,3 +2,10 @@ $('.owl-carousel').owlCarousel({
     loop:false,    
     items:1   
 })
+$(document).ready(function() {
+	    $("#content").fancybox({
+	        'titlePosition' : 'inside',
+	        'transitionIn' : 'fade',
+	        'transitionOut' : 'elastic'
+	    });
+	});
