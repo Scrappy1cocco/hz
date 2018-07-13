e3a1b42eadf7ba0dc30197b8b4a0daed81f83c59
Scrappy1cocco <jedediah.shine100@gmail.com>
2018-07-13 14:22:40 +0300

    ul slider done

--- nerds/js/slide.js
+++ nerds/js/slide.js
@@ -2,3 +2,18 @@ $('.owl-carousel').owlCarousel({
     loop:false,    
     items:1   
 })
+
+function initMap() {
+		  var uluru = {lat: 59.9387, lng: 30.3238};
+		  var mid = {lat: 59.9391642, lng: 30.3216};
+		  var map = new google.maps.Map(
+		      document.getElementById('map'), {zoom: 17, center: mid});
+		  var marker = new google.maps.Marker({
+		  	  position: uluru,
+		  	  map: map,
+		  	  title: 'Nerds',
+			  icon: {
+				url: "nerds-img/content/map-marker.png"
+			}
+		 });
+	}
